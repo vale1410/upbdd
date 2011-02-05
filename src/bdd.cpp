a22ae0eb6ff1eac5024a89d2edc3d369c8ea24d3
Valentin Mayer-Eichberger <vale1410@gmail.com>
2011-02-05 07:36:59 +0100

    backend finished, grasping boost::unordered_set, however problems with templatisation

--- src/bdd.cpp
+++ src/bdd.cpp
@@ -1,117 +1,244 @@
 #include <iostream>
 #include <string>
 #include <sstream>
-#include <stdlib.h>
+//#include <stdlib.h>
 #include <bitset>
-#include <math.h>
+//#include <math.h>
+#include <boost/shared_ptr.hpp>
+#include <boost/unordered_map.hpp>
+#include <boost/functional/hash.hpp>
 
-template<int N>
 class Imp;
+class Bdd;
+class ImpHashFunction;
+class BddHashFunction;
 
-template<int M,int N>
-class Bdd {
-    public:
-    Bdd(unsigned int level,Imp<N>* hImp,Imp<N>* lImp) :
-        _level(level),
-        _function(0x0F0F),
-        _high(NULL),
-        _hImp(hImp),
-        _low(NULL),
-        _lImp(lImp)
-        {
-            
-        }
+typedef unsigned int Level;
+typedef const Imp* ImpP;
+typedef const Bdd* BddP;
 
-        const unsigned int _level;
-        std::bitset<M> _function;
-        
-        const Bdd<M,N>* _high;
-        const Imp<N>* _hImp;
-        const Bdd<M,N>* _low;
-        const Imp<N>* _lImp;
-        
-        std::string toString() {
-            std::ostringstream oss;
-            oss << this << " = ";
-            oss << "(";
-            oss << _level;
-            oss <<  ",";
-            oss << _function;
-            oss <<  ",";
-            oss << _high;
-            oss <<  ",";
-            oss << _hImp;
-            oss <<  ",";
-            oss << _low;
-            oss <<  ",";
-            oss << _lImp;
-            oss <<  ")";
-            return oss.str();
-        }
-};
 
-template<int N>
 class Imp {
     public:
-        Imp():
-        _level(0),
-        _imp(0x0F0F01),
-        next(NULL)
+
+
+    Imp(const Level level, std::bitset<2*8> imp, ImpP next):
+        _level(level),
+        _imp(imp),
+        _next(next)
+    {}
+
+    const Level _level;
+    const std::bitset<2*8> _imp;
+    const ImpP _next;
+    
+    std::string toString() const {
+        std::ostringstream oss;
+        oss << this << " = " << "(" << _level <<  "," << _imp <<  "," << _next <<  ")";
+        return oss.str();
+    }
+
+
+};
+
+
+class Bdd {
+    public:
+        
+    Bdd(const Level level, BddP high, BddP low, ImpP hImp, ImpP lImp) :
+        _level(level),
+        _high(high),
+        _low(low),
+        _hImp(hImp),
+        _lImp(lImp) 
         {}
 
-        const unsigned int _level;
-        const std::bitset<2 * N> _imp;
-        const Imp<N>* next;
-        
-        std::string toString() {
-            std::ostringstream oss;
-            oss << this << " = ";
-            oss << "(";
-            oss << _level;
-            oss <<  ",";
-            oss << _imp;
-            oss <<  ",";
-            //oss << neg;
-            //oss <<  ",";
-            oss << next;
-            oss <<  ")";
-            return oss.str();
+     const Level _level;
+     const BddP _high;
+     const BddP _low;
+     const ImpP _hImp;
+     const ImpP _lImp;
+     
+     std::string toString() const {
+         std::ostringstream oss;
+         oss << this << " = " << "(" << _level <<  "," << _high <<  "," << _low <<  "," << _hImp <<  "," << _lImp <<  ")";
+         return oss.str();
+     }
+};
+    
+
+
+const BddP bddOne = NULL;
+const ImpP impOne = NULL;
+
+
+class Storage;
+
+class ImpHashFunction : std::unary_function< Imp , size_t > {
+    public:
+        inline size_t operator() (const Imp& imp) const {
+            std::size_t hash = 0;
+            boost::hash_combine(hash, imp._level);
+            boost::hash_combine(hash, imp._imp.to_ulong());
+            boost::hash_combine(hash, imp._next);
+            return hash;
         }
 };
 
-template<int N>                 
-void printImp(Imp<N>& imp) 
+class BddHashFunction : std::unary_function< Bdd , size_t > {
+    public:
+        inline size_t operator() (const Bdd& bdd) const {
+            std::size_t hash = 0;
+            boost::hash_combine(hash, bdd._level);
+            boost::hash_combine(hash, bdd._high);
+            boost::hash_combine(hash, bdd._low);
+            boost::hash_combine(hash, bdd._hImp);
+            boost::hash_combine(hash, bdd._lImp);
+            return hash;
+        }
+};
+
+class BddEqual : std::equal_to< Bdd > {
+    public:
+        bool operator() (const Bdd& a, const Bdd& b) const {
+            return (a._level ==  b._level &&
+                    a._high  ==  b._high  &&
+                    a._low   ==  b._low   &&
+                    a._hImp  ==  b._hImp  &&
+                    a._lImp  ==  b._lImp);
+        }
+};
+
+
+class ImpEqual : std::equal_to< Imp > {
+    public:
+        bool operator() (const Imp& a, const Imp& b) const {
+            return (a._level ==  b._level &&
+                    a._imp   ==  b._imp  &&
+                    a._next  ==  b._next);
+        }
+};
+
+inline void printImp(ImpP imp)
 {
-    std::cout << "imp: " << imp.toString() << std::endl;
+    ImpHashFunction hasher;
+    std::cout << "imp: " << imp->toString() << "\t hash: " << hasher(*imp) << std::endl;
 }
 
-template<int M,int N>                 
-void printBdd(Bdd<M,N>& bdd) 
+inline void printBdd(BddP bdd)
 {
-    std::cout << "bdd: " << bdd.toString() << std::endl;
+    BddHashFunction hasher;
+    std::cout << "bdd: " << bdd->toString() << "\t hash: " << hasher(*bdd) << std::endl;
 }
+
+typedef boost::unordered_map< Bdd, BddP, BddHashFunction, BddEqual > BddStoreT;
+typedef boost::unordered_map< Imp, ImpP, ImpHashFunction, ImpEqual > ImpStoreT;
+
+class BddStore {
+    public: 
+
+        BddStore(unsigned long size):
+             store(size)
+        {}
+
+        bool lookup(const Bdd bdd) const {
+            return store.find(bdd) != store.end();
+        }  
+
+        BddP add(const Bdd bdd)  {
+            BddStoreT::iterator i = store.find(bdd);
+            BddP result;
+            if (i != store.end() ) {
+                result = i->second;
+            } else {
+                result = new Bdd(bdd);
+                store[bdd]=result;
+            }            
+            return result;
+        }
+    
+    private:
+        BddStoreT store;
+};
+
+class ImpStore {
+    public: 
+
+        ImpStore(unsigned long size):
+             store(size)
+        {}
+
+        bool lookup(const Imp bdd) const {
+            return store.find(bdd) != store.end();
+        }  
+
+        ImpP add(const Imp bdd)  {
+            ImpStoreT::iterator i = store.find(bdd);
+            ImpP result;
+            if (i != store.end() ) {
+                result = i->second;
+            } else {
+                result = new Imp(bdd);
+                store[bdd]=result;
+            }            
+            return result;
+        }
+    
+    private:
+        ImpStoreT store;
+};
+
+class Backend {
+    public:
+        Backend(unsigned int bddSize, unsigned int impSize):
+            bddStore(bddSize),
+            impStore(impSize)
+        {}
+
+        inline bool lookup(const Bdd bdd) const {
+            return bddStore.lookup(bdd);
+        }
+
+        inline BddP add(const Bdd bdd) {
+            return bddStore.add(bdd);
+        }
+
+        inline bool lookup(const Imp imp) const {
+            return impStore.lookup(imp);
+        }
+
+        inline ImpP add(const Imp imp) {
+            return impStore.add(imp);
+        }
+    private:
+        BddStore bddStore;
+        ImpStore impStore;
+};
 
 
 int main () 
 {
-    unsigned int level = 5;
-    const unsigned int size = 4;
-    const unsigned int sizeBdd = 24;//pow(2,4);
-    std::cout << sizeBdd << std::endl;
-    Imp<size> imp;
-    Bdd<sizeBdd,size> bdd(level,&imp,&imp);
+    const Level level = 0;
+    const std::bitset<16> bits = 0xFF;
+    const Imp imp(level,bits,impOne);
+    const Bdd bdd1(level,bddOne,bddOne,impOne,&imp);
+    const Bdd bdd2(level+1,&bdd1,bddOne,impOne,&imp);
+    const Bdd bdd(level+2,&bdd1,&bdd2,impOne,&imp);
     std::cout << "first tests for bdds" << std::endl;
-    printImp(imp);
-    printBdd(bdd);
-    int a;
-    long b;
-    size_t c;
-    std::cout << "size of bdd " << sizeof(bdd) << std::endl;
+    printImp(&imp);
+    printBdd(&bdd1);
+    printBdd(&bdd2);
+    printBdd(&bdd);
+    //int a;
+    //long b;
+    //size_t c;
     std::cout << "size of imp " << sizeof(imp) << std::endl;
-    std::cout << "size of imp.imp " << sizeof(imp._imp) << std::endl;
-    std::cout << "size of function " << sizeof(bdd._function) << std::endl;
-    std::cout << "size of int " << sizeof(a) << std::endl;
-    std::cout << "size of long " << sizeof(b) << std::endl;
-    std::cout << "size of size_t " << sizeof(c) << std::endl;
+    std::cout << "size of bdd " << sizeof(bdd) << std::endl;
+    //std::cout << "size of imp.imp " << sizeof(imp._imp) << std::endl;
+    //std::cout << "size of int " << sizeof(a) << std::endl;
+    //std::cout << "size of long " << sizeof(b) << std::endl;
+    //std::cout << "size of size_t " << sizeof(c) << std::endl;
+    std::cout << "output of bddOne " << bddOne << std::endl;
+    std::cout << "output of impOne " << impOne << std::endl;
 } 
 
